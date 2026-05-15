4ea415b306abef5831ffba729e1edfc9912d649e
Soy Isaac <isaipaucarchuquispuma@gmail.com>
2026-05-14 22:03:07 -0500

    chore: reorganizar rutas principales en App.tsx y preparar integración de login y rutas protegidas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContex'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import Login from './pages/Login'
 import Home from './pages/Home'
 import Kardex from './pages/Kardex'
 import Historial from './pages/Historial'
 import SaldosIniciales from './pages/SaldosIniciales'
-import { AuthProvider } from './context/AuthContex' // ajusta la ruta según tu proyecto
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/kardex/:procesamiento_id" element={<Kardex />} />
-        <Route path="/historial" element={<Historial />} />
-        <Route path="/saldos" element={<SaldosIniciales />} />
+        {/* Pública */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protegidas */}
+        <Route path="/"                         element={<ProtectedRoute><Home />            </ProtectedRoute>} />
+        <Route path="/kardex/:procesamiento_id" element={<ProtectedRoute><Kardex />          </ProtectedRoute>} />
+        <Route path="/historial"                element={<ProtectedRoute><Historial />       </ProtectedRoute>} />
+        <Route path="/saldos"                   element={<ProtectedRoute><SaldosIniciales />  </ProtectedRoute>} />
       </Routes>
     </AuthProvider>
   )
